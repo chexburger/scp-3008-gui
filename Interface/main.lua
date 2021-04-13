f02268acd414d2cc4f490be39de54b8496deac42
Randall Barker <15860911+chexburger@users.noreply.github.com>
2021-04-13 11:17:17 -0600

    Fix not adding to CoreGui

--- Interface/main.lua
+++ Interface/main.lua
@@ -3,6 +3,9 @@ local UIS = game:GetService("UserInputService")
 local GUI = _G.SCP3008GUI.Interface
 while not GUI do wait() end
 
+syn.protect_gui(GUI)
+GUI.Parent = game:GetService("CoreGui")
+
 local MenuContents = GUI:WaitForChild("Frame"):WaitForChild("MenuContents")
 local MenuSelection = GUI:WaitForChild("Frame"):WaitForChild("MenuSelection")
 
