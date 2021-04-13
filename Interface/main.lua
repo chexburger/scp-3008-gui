8fccd3b61044740112be9161315019d6d8a61829
Randall Barker <15860911+chexburger@users.noreply.github.com>
2021-04-13 15:07:35 -0600

    Add utility function, fix list loading, reset character variable on respawn, added merged item list, finished bring to a test state

--- Interface/main.lua
+++ Interface/main.lua
@@ -14,7 +14,37 @@ local Physical = GameObjects:WaitForChild("Physical")
 local Map = Physical:WaitForChild("Map")
 local Ground = Map:WaitForChild("Ground")
 local Items = Physical:WaitForChild("Items")
-local StructureObjects, Consumables = Ground, Items
+local StructureObjects, Consumables, StructureConsumablesMerged = {}, {}, {}
+
+-- Utility Functions
+function ReloadGroundItemsList()
+  StructureObjects = {}
+  Consumables = {}
+  StructureConsumablesMerged = {}
+  for _,container in pairs(Ground:GetChildren()) do
+    if container:FindFirstChild("Items") then
+      for _,object in pairs(container.Items:GetChildren()) do
+        if object:IsA("Model") and object:FindFirstChild("Base") then
+          table.insert(StructureObjects,object)
+        end
+      end
+    end
+  end
+  for _,item in pairs(Items:GetChildren()) do
+    if item:IsA("Model") and item:FindFirstChildWhichIsA("BasePart") then
+      table.insert(Consumables,item)
+    end
+  end
+  for i,v in pairs(StructureObjects:GetChildren()) do
+    table.insert(StructureConsumablesMerged,v)
+  end
+  for i,v in pairs(Consumables:GetChildren()) do
+    table.insert(StructureConsumablesMerged,v)
+  end
+
+  return StructureObjects, Consumables
+end
+ReloadGroundItemsList()
 
 -- Interface
 local Templates = GUI:WaitForChild("Templates")
@@ -62,8 +92,45 @@ local BringObjectsBringing = false
 local BringObjectsBringingItem = nil
 local BringObjectsBringingCounter = 0 -- Internal, must be 0
 local BringObjectsBringingCount = 0
+local BringObjectsBringingDistance = math.huge()
 
 BringObjects.Visible = true -- Set visible temporary until a menu exists for selecting multiple options
+
+function callBeginObjectBring()
+  if not BringObjectsBringing then return end
+  if not BringObjectsBringingItem then return end
+  if not BringObjectsBringingCount > 0 then return end
+  local hrp = character:FindFirstChild("HumanoidRootPart")
+  if not hrp then return end
+  local pos = hrp.CFrame
+  local movementFunc = character:FindFirstChild("server_PickupSystem"):FindFirstChild("MainEvent")
+
+  local function bringToPosition(item,cf)
+    character:MoveTo(item:FindFirstChildWhichIsA("BasePart").Position)
+      wait(0.15)
+      movementFunc:InvokeServer({
+        ["Action"] = "Pickup",
+        ["Model"] = item
+      });
+      wait(0.15)
+      character:MoveTo(pos.p)
+      wait(0.15)
+      movementFunc:InvokeServer({
+        ["CameraCFrame"] = CFrame.new(0,0,0,0,0,0,0,0,0,0,0,0),
+        ["Action"] = "Drop",
+        ["TargetCFrame"] = cf
+      });
+  end
+
+  for i,v in pairs(StructureConsumablesMerged) do
+    if (v:FindFirstChildWhichIsA("BasePart").Position - pos.p).Magnitude < BringObjectsBringingDistance then
+      if BringObjectsBringingCounter < BringObjectsBringingCount then
+        BringObjectsBringingCounter = BringObjectsBringingCounter + 1
+        bringToPosition(v,pos+Vector3.new(math.random(-3,3),-3,math.random(-3,3)))
+      end
+    end
+  end
+end
 
 BringObjectsOptionsBring.MouseButton1Click:Connect(function()
   if not BringObjectsBringingItem then return end
@@ -71,8 +138,10 @@ BringObjectsOptionsBring.MouseButton1Click:Connect(function()
   BringObjectsOptionsBring.Text = BringObjectsBringing == true and "Abort Bringing" or "Start Bringing"
   if BringObjectsOptionsBring then
     BringObjectsBringingCount = math.ceil(tonumber(BringObjectsOptionsCount.Text) >= 0 and tonumber(BringObjectsOptionsCount.Text) or 0)
+    BringObjectsBringingAmount = math.ceil(tonumber(BringObjectsOptionsDistance.Text) >= 0 and tonumber(BringObjectsOptionsDistance.Text) or math.huge())
   else
     BringObjectsBringingCount = 0
+    BringObjectsBringingDistance = math.huge()
   end
 end)
 
@@ -85,16 +154,18 @@ end
 
 local itemTemplate = Templates:WaitForChild("ObjectTemplate")
 local objectDuplicateList = {}
+local itemDuplicateList = {}
 function reloadBringObjectItemList()
-  duplicateList = {}
-  for _,container in pairs(StructureObjects:GetChildren()) do
-    if container:FindFirstChild("Items") then
-      for _,object in pairs(container.Items:GetChildren()) do
-        if object:IsA("Model") and object:FindFirstChild("Base") then
-          objectDuplicateList[object.Name] = true
-        end
-      end
-    end
+  objectDuplicateList = {}
+  itemDuplicateList = {}
+  for i,v in pairs(BringObjectsObjectList:GetChildren()) do
+    if v:IsA("TextButton") then v:Destroy() end
+  end
+  for i,v in pairs(StructureObjects) do
+    objectDuplicateList[v.Name] = true
+  end
+  for i,v in pairs(Consumables) do
+    itemDuplicateList[v.Name] = true
   end
   for name,_ in pairs(objectDuplicateList) do
     local temp = itemTemplate:Clone()
@@ -104,11 +175,20 @@ function reloadBringObjectItemList()
     temp.Visible = true
     BindBringObjectItemSelect(temp)
   end
+  for name,_ in pairs(itemDuplicateList) do
+    local temp = itemTemplate:Clone()
+    temp.Name = name
+    temp.Text = name
+    temp.Parent = BringObjectsItemList
+    temp.Visible = true
+    BindBringObjectItemSelect(temp)
+  end
 end
 
 reloadBringObjectItemList()
 
-LP.CharacterAdded:Connect(function()
+LP.CharacterAdded:Connect(function(char)
+  character = char
   wait(3)
   reloadBringObjectItemList()
 end)
